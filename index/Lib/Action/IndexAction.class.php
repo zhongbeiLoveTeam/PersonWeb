72b4d31b562690fd1218141a5c3059e7b35ead24
weiwuxiaozi <haoxiangbincool@163.com>
2014-10-26 17:32:41 +0800

    Add  a way of data select on the pages

--- index/Lib/Action/IndexAction.class.php
+++ index/Lib/Action/IndexAction.class.php
@@ -11,13 +11,17 @@
 // 本类由系统自动生成，仅供测试用途
 class IndexAction extends BaseAction {
     public function index(){
-    	/*
-    	 * 个人信息提取
-    	 */
-    	$this->assign('myfrofile',M('Profile')->select());
+    	/*  个人信息提取      */
+    	$this->assign('myfrofile',M('Person')->select());
     	//友情链接
     	$this->assign('link',M('Link')->where('status=1')->order('sort DESC')->select());
 		$this->display();
+		/*   简历信息提取        */
+		$this->assign('myexperience',M('Resume')->select());
+		/*   下载内容提取       */
+		$this->assign('myinformation',M('Material')->select());
+		/*   相册模块数据提取        */
+		$this->assign('myexperience',M('Resume')->select());
     }
     public function map(){
     	//加载map页面
